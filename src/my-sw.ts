43497c98a71f72939ab3f3c213d75e6b00071b24
TsukemonoGit <102149418+TsukemonoGit@users.noreply.github.com>
2023-12-09 18:13:02 +0900

    fix

--- src/my-sw.ts
+++ src/my-sw.ts
@@ -1,1 +1,9 @@
+// sw.js ファイル内の例
+import { precacheAndRoute } from 'workbox-precaching';
+
+// precacheAndRoute メソッドを使ってキャッシュを設定する
+precacheAndRoute([
+	{ url: '/' }
+	// 他のプリキャッシュしたいリソースも追加
+]);
 console.log('[service worker]');
