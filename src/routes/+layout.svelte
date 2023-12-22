5a692cc0adeba46b1ecf782fa3130a2770790abf
TsukemonoGit <102149418+TsukemonoGit@users.noreply.github.com>
2023-12-22 16:59:21 +0900

    test

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -44,24 +44,6 @@
 <title>{$_('main.title')}</title>
 <svelte:head>
 	{@html webManifestLink}
-
-	<!-- Google tag (gtag.js) -->
-
-	<!-- <script
-		async
-		src="https://www.googletagmanager.com/gtag/js?id=G-Y4HDVYX0Z6"
-	></script>
-	<script>
-		window.dataLayer = window.dataLayer || [];
-		function gtag() {
-			dataLayer.push(arguments);
-		}
-		gtag('js', new Date());
-		gtag('config', 'G-Y4HDVYX0Z6', {
-			ad_storage: 'denied',
-			analytics_storage: 'denied'
-		});
-	</script> -->
 </svelte:head>
 
 {#if mounted && pwaInfo}
