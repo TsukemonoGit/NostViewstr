c73e7a45c4442f7e4237efa511dea5f59b5d8634
TsukemonoGit <102149418+TsukemonoGit@users.noreply.github.com>
2023-12-22 19:50:11 +0900

    test

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -41,18 +41,24 @@
 		}
 	});
 
-	$: location =
-		typeof window !== 'undefined' ? window?.location.href : undefined;
-	$: {
-		if (
-			location &&
-			typeof window !== 'undefined' &&
-			(window as any).goatcounter
-		) {
-			(window as any).goatcounter.path = location;
+	// $: location =
+	// 	typeof window !== 'undefined' ? window?.location.pathname : undefined;
+	// $: {
+	// 	if (
+	// 		location &&
+	// 		typeof window !== 'undefined' &&
+	// 		(window as any).goatcounter
+	// 	) {
+	// 		(window as any).goatcounter.path = location;
+	// 		// 他の設定があればここで追加
+	// 	}
+	// }
+	afterNavigate(() => {
+		if (typeof window !== 'undefined' && (window as any).goatcounter) {
+			(window as any).goatcounter.path = window?.location.pathname;
 			// 他の設定があればここで追加
 		}
-	}
+	});
 </script>
 
 <title>{$_('main.title')}</title>
