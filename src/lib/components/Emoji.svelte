f75e0b6695e234e36a93c49bb761b68d53ddead6
TsukemonoGit <102149418+TsukemonoGit@users.noreply.github.com>
2024-03-10 16:36:41 +0900

    emoji fix

--- src/lib/components/Emoji.svelte
+++ src/lib/components/Emoji.svelte
@@ -8,7 +8,7 @@
 	<div class="m-2">
 		{#if iconView}
 			<img
-				class="max-w-16 max-h-16 object-contain justify-center"
+				class="max-w-16 max-h-16 h-16 object-contain justify-center"
 				src={tagArray[2]}
 				alt={tagArray[1]}
 			/>
