e600d7ff904598929b4df26dd64a65840ee54245
TsukemonoGit <102149418+TsukemonoGit@users.noreply.github.com>
2024-03-27 19:09:43 +0900

    一ページに表示するアイテム数修正

--- src/lib/stores/pagination.ts
+++ src/lib/stores/pagination.ts
@@ -2,4 +2,4 @@ import { writable } from 'svelte/store';
 
 export const listSize = writable<number>(0);
 export const pageNum = writable<number>(0);
-export const amount = writable<number>(4);
+export const amount = writable<number>(50);
