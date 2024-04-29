4fc92f44cd809c0688082220b4615ceb8c4cb2fe
TsukemonoGit <102149418+TsukemonoGit@users.noreply.github.com>
2024-04-29 19:55:45 +0900

    fix addnote reference layout

--- src/lib/components/modals/Add/AddTypeRefelence.svelte
+++ src/lib/components/modals/Add/AddTypeRefelence.svelte
@@ -29,7 +29,34 @@
 	<span class="dot" /><span class="px-1 font-bold">Reference</span>
 </article>
 
-<div class="input1-group input1-group-divider grid-cols-[auto_1fr] m-2">
+<div class="m-2">
+	<div class="input-group input-group-divider grid-cols-[auto_1fr_auto] my-1">
+		<div class="input-group-shim" style="padding-left: 4px;padding-right: 4px">
+			Name
+		</div>
+		<input
+			class="input px-1 py-1"
+			type="text"
+			placeholder="link name"
+			bind:value={input2}
+		/>
+	</div>
+	<div class="input-group input-group-divider grid-cols-[auto_1fr_auto] mt-2">
+		<div
+			class="input-group-shim"
+			style="padding-left: 11px;padding-right: 11px"
+		>
+			URL
+		</div>
+		<input
+			class="input px-1 py-1"
+			type="text"
+			placeholder="http://"
+			bind:value={input1}
+		/>
+	</div>
+</div>
+<!-- <div class="input1-group input1-group-divider grid-cols-[auto_1fr] m-2">
 	<label class="label mt-1">
 		<span>Link Name (not required)</span>
 		<input
@@ -48,7 +75,7 @@
 			bind:value={input1}
 		/></label
 	>
-</div>
+</div> -->
 <footer class=" gap-2 flex flex-wrap justify-end mt-2">
 	{#if bkm !== undefined}
 		<!--編集のとき-->
