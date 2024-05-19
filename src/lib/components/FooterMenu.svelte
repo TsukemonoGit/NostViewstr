9ce35f3f9062d6b329769afdf081bcfdca3cf612
TsukemonoGit <102149418+TsukemonoGit@users.noreply.github.com>
2024-05-19 11:11:07 +0900

    未ログインのときだけ設定ボタンのとこにマーク付けてみる

--- src/lib/components/FooterMenu.svelte
+++ src/lib/components/FooterMenu.svelte
@@ -611,7 +611,35 @@
 				<!-- <button class={buttonClass}>{@html updateIcon}</button> -->
 
 				<button class={buttonClass} on:click={onClickInfo}
-					>{@html Setting}</button
+					><div class="relative inline-block">
+						<span
+							class="badge-icon-custom {$pubkey_viewer
+								? 'text-success-500'
+								: 'text-warning-500'} absolute -top-0.5 -right-0.5 z-10"
+							>{#if !$pubkey_viewer}<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="1em"
+									height="1em"
+									viewBox="0 0 24 24"
+									><path
+										fill="currentColor"
+										d="M12 22q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22m0-2q3.35 0 5.675-2.325T20 12t-2.325-5.675T12 4T6.325 6.325T4 12t2.325 5.675T12 20m0 0q-3.35 0-5.675-2.325T4 12t2.325-5.675T12 4t5.675 2.325T20 12t-2.325 5.675T12 20"
+									/></svg
+								>
+								<!-- {:else}
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="32"
+									height="32"
+									viewBox="0 0 24 24"
+									><path
+										fill="currentColor"
+										d="m10.6 16.6l7.05-7.05l-1.4-1.4l-5.65 5.65l-2.85-2.85l-1.4 1.4zM12 22q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22"
+									/></svg
+								> -->
+							{/if}</span
+						>{@html Setting}
+					</div></button
 				>
 				<!-- <button
 				class="btn-icon variant-filled-surface pageIcon"
@@ -680,5 +708,22 @@
 		width: 2em;
 		height: 2em;
 		fill: white;
+	}
+
+	.badge-icon-custom {
+		display: flex;
+		height: 0.8rem /* 20px */;
+		width: 0.8rem /* 20px */;
+		align-items: center;
+		justify-content: center;
+		border-radius: 9999px;
+		font-size: 0.75rem /* 12px */;
+		line-height: 1rem /* 16px */;
+		font-weight: 600;
+		--tw-shadow: 0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1);
+		--tw-shadow-colored: 0 1px 3px 0 var(--tw-shadow-color),
+			0 1px 2px -1px var(--tw-shadow-color);
+		box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000),
+			var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow);
 	}
 </style>
