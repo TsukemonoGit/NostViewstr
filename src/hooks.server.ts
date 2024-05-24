22e2a491d31906cebce5302a2ac3f7388a6461c3
TsukemonoGit <102149418+TsukemonoGit@users.noreply.github.com>
2024-05-24 11:40:51 +0900

    fix

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,7 @@
 // hooks.server.ts
 import type { Handle } from '@sveltejs/kit';
 import { locale } from 'svelte-i18n';
-import { get } from 'svelte/store';
-/** @type {import('@sveltejs/kit').Handle} */
+
 export const handle: Handle = async ({ event, resolve }) => {
 	const lang = event.request.headers.get('accept-language')?.split(',')[0];
 	if (lang) {
