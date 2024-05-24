65aeecc7333965d50ad9156bce0e3c690369a0d8
TsukemonoGit <102149418+TsukemonoGit@users.noreply.github.com>
2024-05-24 11:40:30 +0900

    lang

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,11 +1,16 @@
 // hooks.server.ts
-import type { Handle } from '@sveltejs/kit'
-import { locale } from 'svelte-i18n'
-
+import type { Handle } from '@sveltejs/kit';
+import { locale } from 'svelte-i18n';
+import { get } from 'svelte/store';
+/** @type {import('@sveltejs/kit').Handle} */
 export const handle: Handle = async ({ event, resolve }) => {
-	const lang = event.request.headers.get('accept-language')?.split(',')[0]
+	const lang = event.request.headers.get('accept-language')?.split(',')[0];
 	if (lang) {
-		locale.set(lang)
+		locale.set(lang);
+		return resolve(event, {
+			transformPageChunk: ({ html }) => html.replace('%lang%', lang)
+		});
+	} else {
+		return resolve(event);
 	}
-	return resolve(event)
-}
+};
