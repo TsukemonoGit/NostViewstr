b5a1378a0196a00109edfdae97853cef057b0b6f
TsukemonoGit <102149418+TsukemonoGit@users.noreply.github.com>
2024-07-14 08:54:07 +0900

    ライブラリいろいろアップデート

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -7,12 +7,12 @@ const config = {
 	extensions: ['.svelte'],
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [ vitePreprocess()],
-	
+	preprocess: [vitePreprocess()],
+
 	kit: {
 		// files: {
-    //   serviceWorker: 'src/my-sw.ts', // or `src/my-sw.ts`
-    // },
+		//   serviceWorker: 'src/my-sw.ts', // or `src/my-sw.ts`
+		// },
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
